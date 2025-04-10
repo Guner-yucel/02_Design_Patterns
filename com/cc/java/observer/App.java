0b0214b37171d46c80ff785d8a8f04703a426a50
Guner-yucel <guener.yuecel@outlook.de>
2025-04-10 10:57:42 +0200

    Update App.java

--- com/cc/java/observer/App.java
+++ com/cc/java/observer/App.java
@@ -8,10 +8,39 @@ public class App {
 
 public static void main(String[] args) {
     
+ // Objekte instanziieren
+        Subject s = new Subject();
+        Observer ob1 = new Observer(s, "Bob");
+        Observer ob2 = new Observer(s, "Alice");
+        Observer ob3 = new Observer(s, "Maxine");
 
-       
+        // Bob & Alice abonnieren den Kanal ...
+        ob1.subscribe();
+        ob2.subscribe();
 
-}
+        s.setState("Neuer Post auf IG");
+        Helper.output("---------------");
+
+        ob1.setPushed(true); // Max aktiviert die Glocke!
+        s.setState("Neuer Live-Stream!");
+        Helper.output("---------------");
+
+        // FOMO? : Maxine abonniert ...
+        ob3.subscribe();
+        s.setState("Neuer Post über Solana");
+        Helper.output("---------------");
+
+        //  Maxine kündigt Ihr Abo wieder ...
+        s.detatch(ob3);  
+
+
+        s.setState("Neuer Post über Polkadot");
+        Helper.output("---------------");
+
+
+
+     
+
 
 
 }
